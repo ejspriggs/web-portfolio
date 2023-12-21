2de277ae27cfdb0e017c48522cf3df9114ecee8e
Edward Spriggs <ejspriggs@gmail.com>
2023-12-21 13:21:24 -0500

    Implemented section-change click handlers for placeholder header links, and added resume download link to header.  Still needs actual resume.

--- app.js
+++ app.js
@@ -6,5 +6,17 @@ function lightMode() {
     document.body.className = "colors-light";
 }
 
+function selectFirst() {
+    document.getElementById("first").style.display = "block";
+    document.getElementById("second").style.display = "none";
+}
+
+function selectSecond() {
+    document.getElementById("first").style.display = "none";
+    document.getElementById("second").style.display = "block";
+}
+
 document.getElementById("switch-light").addEventListener("click", lightMode);
 document.getElementById("switch-dark").addEventListener("click", darkMode);
+document.getElementById("select-first").addEventListener("click", selectFirst);
+document.getElementById("select-second").addEventListener("click", selectSecond);
